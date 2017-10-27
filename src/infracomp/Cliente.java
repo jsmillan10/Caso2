5fdbc66c81f28eec83ec1306ab157eafbe273da4
js.millan10 <js.millan10@ISC319.sis.virtual.uniandes.edu.co>
2017-10-27 18:51:47 -0500

    Avances

--- src/infracomp/Cliente.java
+++ src/infracomp/Cliente.java
@@ -1,10 +1,56 @@
 package infracomp;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Cliente {
+
+	boolean ejecutar = true;
+	Socket sock = null;
+	PrintWriter escritor = null;
+	BufferedReader lector = null;
+
+	public Cliente()
+	{
+		try {
+			sock = new Socket("localhost", 8083);
+			escritor = new PrintWriter(sock.getOutputStream(), true);
+			lector = new BufferedReader(new InputStreamReader(
+					sock.getInputStream()));
+			comenzarComunicacion(lector, escritor);
+		} catch (Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+			System.exit(1);
+		}
+	}
+	// cierre el socket y la entrada estándar
+
+	public void comenzarComunicacion(BufferedReader lector, PrintWriter escritor) throws Exception
+	{
+		escritor.println("HOLA");
+		String respuesta = lector.readLine();
+		System.out.println(respuesta);
+		if(respuesta.equals("OK"))
+		{
+			escritor.println("ALGORITMOS:AES:RSA:HMACMD5");
+			respuesta = lector.readLine();
+			System.out.println(respuesta);
+			if(respuesta.equals("OK"))
+			{
+				escritor.println("CERTCLNT");
+			}
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Cliente();
 	}
 
 }
