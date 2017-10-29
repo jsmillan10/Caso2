4d465f5769f422310bfc09868c25f4d8e6546b63
Fernando Reyes Bejarano <f.reyes948@uniandes.edu.co>
2017-10-29 03:18:58 -0500

    Esqueleto de los pasos 4 a 6. No funciona el reto ni la autenticación.

--- src/infracomp/Cliente.java
+++ src/infracomp/Cliente.java
@@ -6,25 +6,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.math.BigInteger;
 import java.net.Socket;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 import java.util.Date;
+import java.util.Random;
 
 import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.jce.X509Principal;
 import org.bouncycastle.jce.provider.X509CertificateObject;
+import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.util.io.pem.PemObject;
 import org.bouncycastle.util.io.pem.PemObjectGenerator;
+import org.bouncycastle.util.io.pem.PemReader;
 import org.bouncycastle.util.io.pem.PemWriter;
 import org.bouncycastle.x509.X509V3CertificateGenerator;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import javax.security.auth.x500.X500Principal;
 import javax.xml.bind.DatatypeConverter;
 
@@ -34,6 +48,12 @@ public class Cliente {
 	Socket sock = null;
 	PrintWriter escritor = null;
 	BufferedReader lector = null;
+	PrivateKey priKey = null;
+	PublicKey pubKey = null;
+	SecretKey simKey = null;
+	
+	private final static String ALGORITMO_ASIM = "RSA";
+	private final static String ALGORITMO_SIM = "AES/ECB/PKCS5Padding";
 
 	public Cliente()
 	{
@@ -42,15 +62,16 @@ public class Cliente {
 			escritor = new PrintWriter(sock.getOutputStream(), true);
 			lector = new BufferedReader(new InputStreamReader(
 					sock.getInputStream()));
-			comenzarComunicacion(lector, escritor);
+			comenzarComunicacion();
 		} catch (Exception e) {
+			e.printStackTrace();
 			System.err.println("Exception: " + e.getMessage());
 			System.exit(1);
 		}
 	}
-	// cierre el socket y la entrada est�ndar
+	// cierre el socket y la entrada est�ndar
 
-	public void comenzarComunicacion(BufferedReader lector, PrintWriter escritor) throws Exception
+	public void comenzarComunicacion() throws Exception
 	{
 		escritor.println("HOLA");
 		String respuesta = lector.readLine();
@@ -62,20 +83,35 @@ public class Cliente {
 			System.out.println(respuesta);
 			if(respuesta.equals("OK"))
 			{
+				
 				String certHecho = generarCertificado();
 				escritor.println("CERTCLNT:" + certHecho);
 				String certRecibido = "";
+				certRecibido=lector.readLine().split(":")[1] + "\n";
 				while(!(respuesta=lector.readLine()).contains("END CERTIFICATE"))
 				{
-					certRecibido+=respuesta;
+					certRecibido+= respuesta;
 				}
-				int reto = (int) (Math.random()*10000+1);
-				//Hay que revisar que se le env�e al servidor una cadena de n�meros par como 01 o 4875 o 195723
-				System.out.println(reto);
-				byte[] hexa = DatatypeConverter.parseHexBinary(""+reto);
-				escritor.println(reto);
-				respuesta = lector.readLine();
-				System.out.println(respuesta);
+				certRecibido+="\n" +respuesta;
+				
+				StringReader inPem = new StringReader(certRecibido);
+				
+//				PemReader pemRea = new PemReader(inPem);
+//				PemObject pemObj = pemRea.readPemObject();
+//				pemRea.close();
+				
+//				PemObject pemObj = 
+				
+				PEMParser pemPar = new PEMParser(inPem);
+				X509CertificateHolder servCert = (X509CertificateHolder) pemPar.readObject();
+				pemPar.close();
+				pubKey = new JcaX509CertificateConverter().getCertificate( servCert ).getPublicKey();
+				System.out.println(pubKey);
+				
+				reto();
+				
+				autenticar();
+				
 			}
 		}
 
@@ -88,7 +124,7 @@ public class Cliente {
 		Date validityEndDate = new Date(System.currentTimeMillis() + 2 * 365 * 24 * 60 * 60 * 1000);
 
 		// GENERATE THE PUBLIC/PRIVATE RSA KEY PAIR
-		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(ALGORITMO_ASIM);
 		keyPairGenerator.initialize(1024, new SecureRandom());
 
 		KeyPair keyPair = keyPairGenerator.generateKeyPair();
@@ -109,6 +145,7 @@ public class Cliente {
 		v3Cert.setSignatureAlgorithm("SHA256withRSA");
 
 		X509Certificate cert = v3Cert.generate(keyPair.getPrivate());
+		priKey = keyPair.getPrivate();
 		StringWriter out = new StringWriter();
 		PemWriter pem = new PemWriter(out);
 		pem.writeObject(new PemObject("CERTIFICATE", cert.getEncoded()));
@@ -122,6 +159,97 @@ public class Cliente {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Cliente();
+	}
+	
+	/**
+	 * Envío y confirmación del reto.
+	 */
+	public void reto() {
+		try {
+			Random rand = new Random();
+			rand.setSeed(System.currentTimeMillis());
+			int reto = (int) (rand.nextInt());
+			while (!(String.valueOf(reto).length() % 2 == 0)) {
+				//Hay que revisar que se le envíe al servidor una cadena 
+				//de números con un número par de dígitos, como 01 o 4875 o 195723.
+				reto = (int) (rand.nextInt()*10000+1);
+			}
+			System.out.println(reto);
+			Cipher cipher = Cipher.getInstance(ALGORITMO_ASIM);
+			cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+			String hexa = DatatypeConverter.printHexBinary(cipher.doFinal(String.valueOf(reto).getBytes()));
+			escritor.println(hexa);	
+			String respuesta = "";
+			respuesta = lector.readLine();
+			respuesta = lector.readLine();
+//			System.out.println(Integer.parseInt(respuesta, 16));
+			escritor.println("OK");
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void autenticar() {
+		try {
+			String respuesta = lector.readLine();
+			Cipher cipher = Cipher.getInstance(ALGORITMO_ASIM);
+			cipher.init(Cipher.DECRYPT_MODE, priKey);
+			byte[] data = DatatypeConverter.parseHexBinary(respuesta);
+			simKey = new SecretKeySpec(cipher.doFinal(data),ALGORITMO_SIM);
+			System.out.println(simKey);
+			
+			String autStr = "usuario,clave";			
+			cipher = Cipher.getInstance(ALGORITMO_SIM);
+			cipher.init(Cipher.ENCRYPT_MODE, simKey);
+			byte[] cipAut = cipher.doFinal(autStr.getBytes());
+			escritor.println(DatatypeConverter.printHexBinary(cipAut));
+			
+			respuesta = lector.readLine();
+			cipher.init(Cipher.DECRYPT_MODE, simKey);
+			byte[] bRes = cipher.doFinal(DatatypeConverter.parseHexBinary(respuesta));
+			respuesta = bRes.toString();
+			if(!respuesta.equalsIgnoreCase("OK")) {
+				throw new Exception("No se logró autenticar.");
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
